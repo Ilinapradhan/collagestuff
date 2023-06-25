4eb7a4477c273f0a5faf7a54cf9a6d601c85f237
ilinapradhan <ilina04khushi@gmail.com>
2023-06-25 20:28:47 +0530

    footer

---
+++ footer/script.js
@@ -0,0 +1,16 @@
+
+$("#com").click(function(){
+    $("#men").toggle(400);
+  });
+
+  $("#hel").click(function(){
+    $("#men1").toggle(400);
+  });
+
+  $("#foo").click(function(){
+    $("#men2").toggle(400);
+  });
+
+  $("#foll").click(function(){
+    $("#men3").toggle(400);
+  });
